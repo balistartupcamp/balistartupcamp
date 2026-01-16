272e252fa320935d52cc68905725d21608d5cfea
moshcat <mauricecathoverile@gmail.com>
2026-01-16 16:14:31 +0800

    feat: add exception for CSRF token validation for Livewire file uploads

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+        'livewire/upload-file', // Add this line
+        ]);
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
