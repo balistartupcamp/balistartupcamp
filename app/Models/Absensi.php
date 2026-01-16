1501eac6e3631a1f3cf1da03a29080b227cbd961
moshcat <mauricecathoverile@gmail.com>
2026-01-16 10:25:28 +0800

    feat: add Absensi and Attendance models with necessary properties and relationships

---
+++ app/Models/Absensi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Absensi extends Model
+{
+    use HasUuids;
+    protected $table = "absensi";
+    protected $fillable = [
+        'title',
+        'slug',
+        'is_active'
+    ];
+}
---
+++ app/Models/Attendance.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasUuids;
+    protected $table = "attendance";
+    protected $fillable = [
+        'absensi_id',
+        'nama_lengkap',
+        'nim',
+        'program_studi',
+        'nama_startup',
+        'nomor_telepon',
+        'ttd',
+        'bukti_foto',
+    ];
+
+    public function absensi()
+    {
+        return $this->belongsTo(Absensi::class, 'absensi_id');
+    }
+}
