3a6f2e8a2c482a4e82f7561c4f374d33eba79e93
moshcat <mauricecathoverile@gmail.com>
2026-01-16 16:54:22 +0800

    feat: enhance PublicAbsensiForm with file upload support and update routing middleware

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ Route::get("/links", function() {
     return Inertia::render('links');
 });
 
-Route::get('/absen/{absensi:slug}', PublicAbsensiForm::class)->name('public.absen');
+Route::get('/absen/{absensi:slug}', PublicAbsensiForm::class)
+    ->middleware(['web'])
+    ->name('public.absen');
 
 Route::get('/status/success', function () {
     return view('absensi.success');
