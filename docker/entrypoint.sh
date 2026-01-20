5091b0ca0fe8e3dff20b5d770b058cb645ab34ca
I Made Gautama <88269066+imadegautama@users.noreply.github.com>
2026-01-20 11:51:16 +0800

    chore: Add `php artisan filament:optimize` to the entrypoint script.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
 php artisan storage:link --force
 
 php artisan migrate --force
+
 php artisan optimize
+
+php artisan filament:optimize
 
 # Start supervisor (runs Apache + Queue Worker)
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
